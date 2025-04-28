2143a48cf35be2e2c62ac01373ec994ef005506d
Dem-7 <dem67713@gmail.com>
2025-04-28 18:08:01 +0300

    п

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ WEATHER_API_KEY = '2f6355f2f3984db0265265592ad15788'
 CAT_API_KEY = 'live_Im8GjBfy9Ue7GVyCscJGTJzMB4q9HmB57X2Ruak7Ks3QXBOIAxDigZMZh02nMyGr'
 
 # Токен вашего Telegram-бота
-BOT_TOKEN = '7051479860:AAGpiDBFdSjga9SwUpASeebo1AuLBB2e2bk'
+BOT_TOKEN = 'Ваш токен'
 
 # Базовые URL для API
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
